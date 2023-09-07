ae1124a92a03fc3b2ee05f20560060d7b8b4bb5b
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-07 12:12:12 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+* main - this is a program that prints a string using the printf function
+*
+* Return: Always 0
+*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+

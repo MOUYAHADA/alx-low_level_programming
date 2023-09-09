8d10984ba8a7b9a17b8c2bfa83567a6540f452f6
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-08 19:38:59 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet in reverse
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 127; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+
+	return (0);
+}

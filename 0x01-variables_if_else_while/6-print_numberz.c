bb22bf0f3a3318d9562cef9f67f0db49a632f8d0
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-08 19:28:03 -0700

    Print Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - write numbers from 1 to 10
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		putchar(10);
+	}
+
+	return (0);
+}

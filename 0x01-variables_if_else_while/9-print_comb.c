a7bcfee2cb6e9e645ea27c6985e6f8d77039c579
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-08 19:58:14 -0700

    Number, List

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print_comb.c
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		putchar(44);
+		putchar(32);
+	}
+	putchar(10);
+
+	return (0);
+}

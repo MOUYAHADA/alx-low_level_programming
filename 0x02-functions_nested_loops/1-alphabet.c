607a5c4ad918503c85a1d00505517c940194d3d5
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-12 16:49:45 -0700

    _PUTCHAR alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,6 +10,23 @@
 
 int main(void)
 {
-	print_alphabet();
+
+	/**
+	 * print_alphabet - print alphabet
+	 *
+	 * Return: 0
+	 */
+
+	int print_alphabet(void)
+	{
+		int i;
+
+		for (i = 97; i < 123; i++)
+		{
+			putchar(i);
+		}
+		_putchar(10);
+		return (0);
+	}
 	return (0);
 }

7fcf1b356d55d477d8a0833f1c9f420faf684729
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-12 16:42:30 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,37 +4,14 @@
 #include <stdio.h>
 
 /**
- * put_chars - function prints the string `_putchar` to the screen
- * with the help of the function putchar
- * Return: 0
- */
-int put_chars(void)
-{
-	int i[8] = {95, 112, 117, 116, 99, 104, 97, 114}, x = 0;
-
-	for (x = 0; x < 8; x++)
-	{
-		putchar(i[x]);
-	}
-	putchar(10);
-	return (0);
-}
-
-/**
- * print_alphabet - print alphabet
+ * _putchar - print "_puthcar" to the screen
  *
  * Return: 0
  */
 
-int print_alphabet(void)
+int _putchar(char c)
 {
-	int i;
-
-	for (i = 97; i < 123; i++)
-	{
-		putchar(i);
-	}
-	putchar(i);
-	return (0);
+	return (write(1, &c, 1));
 }
+
 #endif

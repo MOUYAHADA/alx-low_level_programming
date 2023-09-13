6be918e87dced827a37921bd283306a1d6990cea
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-13 07:55:40 -0700

    T5 Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_sign - prints the sign of a number
@@ -10,14 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		printf("0");
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		printf("-1");
+		_putchar('-');
+		return (-1);
 	}
 }

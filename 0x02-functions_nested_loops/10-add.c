c00003f1eb317a97a55ccdd7ef95aa8ed1e6d983
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-13 14:37:43 -0700

    T10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - sum two integers ex: a + b
+ * @a: first number
+ * @b: second number
+ * Return: the result of a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}

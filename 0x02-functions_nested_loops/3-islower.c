5854da6b12d0052946c337d7130addbfaea95097
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-13 05:28:52 -0700

    T3 _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "mian.h"
+#include <ctype.h>
+
+/**
+ * _islower - check if letter is lowercase
+ * @c: the letter to be checked
+ * Return: 1 lowercase, 0 = not lowercase
+ */
+
+int _islower(char c)
+{
+	return (islower(ch) ? 1 : 0);
+}

dcbec6c413c14c2d222019a332f55f96cbd810eb
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-13 04:35:51 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,3 +11,20 @@ int main(void)
 	put_chars();
 	return (0);
 }
+
+/**
+ * put_chars - function prints the string `_putchar` to the screen
+ * with the help of the function putchar
+ * Return: 0
+ */
+int put_chars(void)
+{
+	int i[8] = {95, 112, 117, 116, 99, 104, 97, 114}, x = 0;
+
+	for (x = 0; x < 8; x++)
+	{
+		_putchar(i[x]);
+	}
+	_putchar(10);
+	return (0);
+}

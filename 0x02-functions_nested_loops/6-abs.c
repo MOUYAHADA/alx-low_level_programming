fe5144c223b0098bd6a6f44f42a308ca69919f53
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-13 09:16:57 -0700

    T6 _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - compute the absolute value of n
+ * @n: number to check
+ * Return: the absolute value
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}

85cc74d34c82d3735fee98d0ca6876a049eae89e
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-13 09:23:59 -0700

    T7 print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of any given number
+ * @n: given number
+ * Return: value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}

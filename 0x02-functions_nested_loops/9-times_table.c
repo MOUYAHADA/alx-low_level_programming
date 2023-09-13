3557880fbd124c18e159dbcd62fcbf73fc6790a9
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-13 13:34:03 -0700

    T9 times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - print the times table of 0 to 9
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i, x, times;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			times = i * x;
+			if (times > 10)
+			{
+				_putchar(48 + (times / 10));
+				_putchar(48 + (times % 10));
+			}
+			else
+			{
+				_putchar(48 + times);
+			}
+			_putchar(44);
+			_putchar(32);
+		}
+		_putchar(10);
+	}
+}

b06640b4c2fd6b749ff84e6d94af1c7010e13a95
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-14 13:45:35 -0700

    T:5 more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 0-14 x10 times
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i, x;
+
+	for (x = 0; i < 10; i++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar(49);
+			}
+			_putchar(48 + ((i >= 10) ? i - 10 : i));
+		}
+		_putchar(10);
+	}
+}

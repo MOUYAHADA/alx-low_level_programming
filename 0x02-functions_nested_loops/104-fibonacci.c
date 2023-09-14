4f47f305f96db97744b960f4e1e15c79f52f7ed4
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-13 18:20:54 -0700

    T104 fibo_98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,38 +8,36 @@
 
 int main(void)
 {
-	int main(void)
-{
-    unsigned long int x, p, t, p1, p2, t1, t2;
+	unsigned long int x, p, t, p1, p2, t1, t2;
 
-    p = 1;
-    t = 2;
+	p = 1;
+	t = 2;
 
-    printf("%lu", p);
+	printf("%lu", p);
 
-    for (x = 1; x < 91; x++)
-    {
-        printf(", %lu", t);
-        t = t + p;
-        p = t - p;
-    }
+	for (x = 1; x < 91; x++)
+	{
+		printf(", %lu", t);
+		t = t + p;
+		p = t - p;
+	}
 
-    p1 = p / 1000000000;
-    p2 = p % 1000000000;
-    t1 = t / 1000000000;
-    t2 = t % 1000000000;
+	p1 = p / 1000000000;
+	p2 = p % 1000000000;
+	t1 = t / 1000000000;
+	t2 = t % 1000000000;
 
-    for (x = 92; x < 99; ++x)
-    {
-        printf(", %lu", t1 + (t2 / 1000000000));
-        printf("%lu", t2 % 1000000000);
-        t1 = t1 + p1;
-        p1 = t1 - p1;
-        t2 = t2 + p2;
-        p2 = t2 - p2;
-    }
+	for (x = 92; x < 99; ++x)
+	{
+		printf(", %lu", t1 + (t2 / 1000000000));
+		printf("%lu", t2 % 1000000000);
+		t1 = t1 + p1;
+		p1 = t1 - p1;
+		t2 = t2 + p2;
+		p2 = t2 - p2;
+	}
 
-    printf("\n");
+	printf("\n");
 
-    return (0);
-}}
+	return (0);
+}

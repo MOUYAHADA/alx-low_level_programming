744ef8ac67c71799a6c292af28e15ae15f9cd3fc
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-09-25 16:04:14 -0700

    Task 8 - diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,15 +6,17 @@
  * @a: pointer to a
  * @size: size of diagonal
  */
+
 void print_diagsums(int *a, int size)
 {
-    int i, sum_1 = 0, sum_2 = 0;
+	int i, sum_1 = 0, sum_2 = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        sum_1 += *(a + (size * i + i));
-        sum_2 += *(a + (size * i + size - 1 - i));
-    }
-    printf("%d, ", sum_1);
-    printf("%d\n", sum_2);
+	for (i = 0; i < size; i++)
+	{
+		sum_1 += *(a + (size * i + i));
+		sum_2 += *(a + (size * i + size - 1 - i));
+	}
+	printf("%d, ", sum_1);
+	printf("%d\n", sum_2);
 }
+

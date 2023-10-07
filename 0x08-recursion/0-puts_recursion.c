c1eecedc03739296b5e0697e0f027601f77c5900
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-10-07 14:10:34 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,13 @@
 
 void _puts_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 	{
 		_putchar(10);
 	}
-	_putchar(*s);
-	if(*s != '\0')
+	if (*s != '\0')
 	{
+		_putchar(*s);
 		s++;
 		_puts_recursion(s);
 	}

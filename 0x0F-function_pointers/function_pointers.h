d9b4d3fcf528095b86a6d9705615a3ac29b35156
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-10-11 14:24:35 -0700

    array_iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,9 @@
 #ifndef FUNCTION_P
 #define FUNCTION_P
+
+#include <stddef.h>
+
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
 #endif

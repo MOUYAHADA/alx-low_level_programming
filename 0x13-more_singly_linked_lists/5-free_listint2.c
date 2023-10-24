ac261640b523429e4c88aed7f289d6df738c9cce
Mohamed MOUYAHADA <mouyahada@gmail.com>
2023-10-24 16:26:54 -0700

    0x13 free_listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,11 @@
 
 void free_listint2(listint_t **head)
 {
-	if ((*head) == NULL)
+	if (head == NULL)
 		return;
 
-	free_listint((*head)->next);
+	if ((*head)->next != NULL)
+		free_listint2(&((*head)->next));
 	free(*head);
 	*head = NULL;
 }

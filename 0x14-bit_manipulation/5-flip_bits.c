0bd2b8c5076e0426c420fcbe8131fff76545ed09
= <=>
2023-11-24 20:14:23 +0100

    0x14 - 5-flip_bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - count the number of bits we need to flip
+ * to get from one number to an other
+ *
+ * @n: first number
+ *
+ * @m: second number
+ *
+ * Return: number of bits to flip;
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int difference = n ^ m;
+	unsigned int count = 0;
+
+	while (difference > 0)
+	{
+		count += difference & 1;
+		difference >>= 1;
+	}
+	return (count);
+}

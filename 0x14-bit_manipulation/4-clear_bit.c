8007491e738b9456b27a5634c94133d43d01affe
= <=>
2023-11-24 19:43:08 +0100

    0x14 - clear_bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets a bit to 0 at a given index
+ *
+ * @n: number
+ *
+ * @index: index
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n &= ~(mask << index);
+
+	return (1);
+}

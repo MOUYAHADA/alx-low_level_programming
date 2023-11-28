dbc993c2ffc9be760de6bd64b086810a18df5f40
Mohamed Mouyahada <mouyahada@gmail.com>
2023-11-28 04:12:03 +0100

    0x15 - 3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,28 +1,7 @@
-#include "main.h"
-
-
-/**
- * close_fd_all - closes all file descriptors
- *
- * @fd: file descriptior
- *
- * @fd2: file descriptor
- */
-
-void close_fd_all(int fd, int fd2)
-{
-	if (close(fd) == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-	if (close(fd2) == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
-
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
  * close_fd - closes a file descriptor
@@ -34,7 +13,7 @@ void close_fd(int fd)
 {
 	if (close(fd) == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
@@ -52,9 +31,9 @@ void close_fd(int fd)
 void print_error(int errorNo, char *filename, int exitCode)
 {
 	if (errorNo == 1)
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 	if (errorNo == 2)
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
 	exit(exitCode);
 }
 
@@ -70,44 +49,39 @@ void print_error(int errorNo, char *filename, int exitCode)
 
 int main(int ac, char **av)
 {
-	int fd_from, fd_to;
-	ssize_t bytesRead;
-	char *buffer;
+	int fd_from, fd_to, bytesRead;
+	char buffer[1024];
 
 	if (ac != 3)
 	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		close_fd(fd_from);
 		print_error(1, av[1], 98);
-	}
+
+
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		close_fd_all(fd_from, fd_to);
-		print_error(1, av[2], 98);
+		close(fd_from);
+		print_error(2, av[2], 99);
 	}
-	buffer = malloc(sizeof(char) * 1024);
-	do {
-		bytesRead = read(fd_from, buffer, 1024);
-		if (bytesRead == -1)
-		{
-			close_fd_all(fd_from, fd_to);
-			free(buffer);
-			print_error(1, av[1], 98);
-		}
+
+	while (((bytesRead = read(fd_from, buffer, 1024))) > 0)
+	{
 		if (write(fd_to, buffer, bytesRead) == -1)
-		{
-			close_fd_all(fd_from, fd_to);
-			free(buffer);
-			print_error(1, av[1], 98);
-		}
-	} while (bytesRead != 0);
-	free(buffer);
+			print_error(2, av[2], 99);
+	}
+
+	if (bytesRead == -1)
+		print_error(1, av[1], 98);
+
+
+	close_fd(fd_from);
+	close_fd(fd_to);
+
 	return (0);
 }

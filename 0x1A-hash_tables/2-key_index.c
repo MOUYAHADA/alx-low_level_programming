477ed750e2623f755dfe98736d5b3e3af29dc194
Mohamed Mouyahada <mouyahada@gmail.com>
2024-02-03 01:50:19 +0100

    key index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+
+/**
+ * key_index - gets the index of a node using the key
+ *
+ * @key: key
+ * @size: size of the array
+ *
+ * Return: index where the new pair should be stored
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	if (key == NULL || size == 0)
+		return (0);
+
+	index = hash_djb2(key) % size;
+
+	return (index);
+}
